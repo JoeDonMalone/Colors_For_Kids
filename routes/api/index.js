7d0b3fca8baa136fb22bb042fcd116e6e9964815
Joe Malone <joe.d.malone@gmail.com>
2021-09-02 09:01:01 -0500

    Initial build push; backend packages installed, react-redux template added

---
+++ routes/api/index.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+// const colorRoutes = require("./colors");
+
+// Define Specific API Routes here
+// router.use("./colors", colorRoutes);
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Define any middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// static assets 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+}
+
+// add API and view routes
+app.use(routes);
+
+// Connect to MongoDB if exists
+//Future
+
+// Start API Server
+app.listen(PORT, function () {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
